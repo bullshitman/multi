c20cb31186ca78c98557a37fbeb0909008cf3bdc
Eugene Sokolin <sokol.joe@gmail.com>
2018-09-05 20:36:50 +0300

    consts

--- app/src/main/java/com/aparatus/multiapp/MainActivity.java
+++ app/src/main/java/com/aparatus/multiapp/MainActivity.java
@@ -1,12 +1,13 @@
 package com.aparatus.multiapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.vk.sdk.util.VKUtil;
-
-import java.util.Arrays;
+import com.aparatus.multiapp.consts.ApiConstants;
+import com.vk.sdk.VKAccessToken;
+import com.vk.sdk.VKCallback;
+import com.vk.sdk.VKSdk;
+import com.vk.sdk.api.VKError;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VKSdk.login(this, ApiConstants.DEFAULT_LOGIN_SCOPE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                // Пользователь успешно авторизовался
+            }
+            @Override
+            public void onError(VKError error) {
+                // Произошла ошибка авторизации (например, пользователь запретил авторизацию)
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
